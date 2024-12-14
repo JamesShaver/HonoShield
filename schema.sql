d98bc60daad309c84cba053294d71ab2030f4a10
James <shaver.tech@gmail.com>
2024-12-14 09:02:41 -0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -12,4 +12,4 @@ secret_key TEXT,
 created_on datetime default current_timestamp,
 updated_on datetime default current_timestamp
 );
-INSERT INTO users (fistName, lastName, company, username, password, status) VALUES ('James','Shaver','HonoShield','shaver.tech@gmail.com','$2y$10$shvcobnqwtVxjPdZq2U2/OZt2hECWKJjShLK4Vyx0eBO2TNUWR3na', 1);
+INSERT INTO users (fistName, lastName, company, username, password, status) VALUES ('James','Shaver','HonoShield','admin@honoshield.com','$2y$10$shvcobnqwtVxjPdZq2U2/OZt2hECWKJjShLK4Vyx0eBO2TNUWR3na', 1);
