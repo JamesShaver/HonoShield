6480e30c8e9e9f348943fc7dc93e2d021a30b796
James Shaver <shaver.tech@gmail.com>
2024-12-23 04:09:57 -0800

    Schema correction

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-fistName TEXT NOT NULL,
+firstName TEXT NOT NULL,
 lastName TEXT NOT NULL,
 company TEXT NOT NULL,
 username TEXT UNIQUE NOT NULL,
@@ -12,4 +12,4 @@ secret_key TEXT,
 created_on datetime default current_timestamp,
 updated_on datetime default current_timestamp
 );
-INSERT INTO users (fistName, lastName, company, username, password, status) VALUES ('James','Shaver','HonoShield','admin@honoshield.com','$2y$10$shvcobnqwtVxjPdZq2U2/OZt2hECWKJjShLK4Vyx0eBO2TNUWR3na', 1);
+INSERT INTO users (firstName, lastName, company, username, password, status) VALUES ('James','Shaver','HonoShield','admin@honoshield.com','$2y$10$shvcobnqwtVxjPdZq2U2/OZt2hECWKJjShLK4Vyx0eBO2TNUWR3na', 1);
